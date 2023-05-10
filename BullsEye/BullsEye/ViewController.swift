3fe073ad32c05f430d56a01010e3ad4e1b4c0708
bjke <kbjwdh@gmail.com>
2023-05-10 12:22:02 +0800

    05.end

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -1,15 +1,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-  var currentValue: Int = 50
+  @IBOutlet var slider: UISlider!
+  @IBOutlet var targetLabel: UILabel!
+  @IBOutlet var scoreLabel: UILabel!
+  @IBOutlet var roundLabel: UILabel!
 
+  var targetValue = 0
+  var score = 0
+  var currentValue: Int = 50
+  var round = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    startNewRound()
   }
 
   @IBAction func showAlert() {
-    let message = "The value of the slider is: \(currentValue)"
+    let difference = abs(targetValue - currentValue)
+    let points = 100 - difference
+    score += points        // add this line
+    
+    let message = "You scored \(points) points"
 
     let alert = UIAlertController(
       title: "Hello, World",
@@ -23,10 +36,24 @@ class ViewController: UIViewController {
 
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
+    startNewRound()
   }
 
   @IBAction func sliderMoved(_ slider: UISlider) {
     currentValue = lroundf(slider.value)
   }
-}
 
+  func startNewRound() {
+    round += 1
+    targetValue = Int.random(in: 1 ... 100)
+    currentValue = 50
+    slider.value = Float(currentValue)
+    updateLabels()
+  }
+
+  func updateLabels() {
+    targetLabel.text = String(targetValue)
+    scoreLabel.text = String(score)
+    roundLabel.text = String(round)
+  }
+}
