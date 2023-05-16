3ff370388f3a16f46eb9ebd0adb1e77eb0f56dfe
lennon <kbjwdh@gmail.com>
2023-05-16 17:59:09 +0800

    18.end

--- Checklists/Checklists/Models/DataModel.swift
+++ Checklists/Checklists/Models/DataModel.swift
@@ -2,16 +2,38 @@ import Foundation
 
 class DataModel {
   var lists = [Checklist]()
+  var indexOfSelectedChecklist: Int {
+    get {
+      return UserDefaults.standard.integer(
+        forKey: "ChecklistIndex")
+    }
+    set {
+      UserDefaults.standard.set(
+        newValue,
+        forKey: "ChecklistIndex")
+    }
+  }
 
   init() {
     loadChecklists()
+    registerDefaults()
+    handleFirstTime()
+  }
+
+  func registerDefaults() {
+    let dictionary = [
+      "ChecklistIndex": -1,
+      "FirstTime": true
+    ] as [String: Any]
+    UserDefaults.standard.register(defaults: dictionary)
   }
 
   // MARK: - Data Saving
+
   func documentsDirectory() -> URL {
     let paths = FileManager.default.urls(
       for: .documentDirectory,
-         in: .userDomainMask)
+      in: .userDomainMask)
     return paths[0]
   }
 
@@ -44,4 +66,18 @@ class DataModel {
       }
     }
   }
+  
+  func handleFirstTime() {
+    let userDefaults = UserDefaults.standard
+    let firstTime = userDefaults.bool(forKey: "FirstTime")
+
+    if firstTime {
+      let checklist = Checklist(name: "List")
+      lists.append(checklist)
+
+      indexOfSelectedChecklist = 0
+      userDefaults.set(false, forKey: "FirstTime")
+    }
+  }
+
 }
