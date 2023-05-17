f5f7583b3895d4bbe0baa97c7001eaabc6ed657c
lennon <kbjwdh@gmail.com>
2023-05-17 18:12:02 +0800

    init

---
+++ MyLocations/MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by bjke on 2023/5/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
