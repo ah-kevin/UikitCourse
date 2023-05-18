580164d5397a164d61a59dd36e9d5a02c325f0d6
bjke <kbjwdh@gmail.com>
2023-05-18 23:10:14 +0800

    Pass the context

---
+++ MyLocations/MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by bjke on 2023/5/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
