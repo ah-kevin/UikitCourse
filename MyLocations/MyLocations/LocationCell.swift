49eb900b90ca8155b24aa1f1631e887084579c71
lennon <kbjwdh@gmail.com>
2023-05-27 10:44:08 +0800

    add

---
+++ MyLocations/MyLocations/LocationCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCellTableViewCell.swift
+//  MyLocations
+//
+//  Created by bjke on 2023/5/26.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
