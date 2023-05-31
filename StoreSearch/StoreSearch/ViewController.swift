465204a483b42ae6cc16ebee3eaa5ffad58a61d2
bjke <kbjwdh@gmail.com>
2023-05-31 23:10:41 +0800

    04.StoreSearch
    init

---
+++ StoreSearch/StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by bjke on 2023/5/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
